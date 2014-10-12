023f32a8f1c1f280044f5786dc8e9c033e2c3b4f
MichalLukac <cospelthetraceur@gmail.com>
2014-10-12 21:07:18 +0200

    Checking if null.

--- Lib/src/main/java/cz/fi/muni/pa165/dao/IBookDAO.java
+++ Lib/src/main/java/cz/fi/muni/pa165/dao/IBookDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.fi.muni.pa165.dao;
 
 import java.util.List;
@@ -11,7 +6,8 @@ import cz.fi.muni.pa165.entity.Book;
 import cz.fi.muni.pa165.entity.Book.Department;
 
 /**
- *
+ * Dao interface for Book
+ * 
  * @author michal.lukac, xlukac, 430614
  */
 public interface IBookDAO {
