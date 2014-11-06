ccd234c5c7e29a98206fe33cb329f9fe2fccf84d
Martin Malik <374128@mail.muni.cz>
2014-11-06 02:27:28 +0100

    service member

--- library-service/src/main/java/cz/fi/muni/pa165/datatransferobject/MemberDTO.java
+++ library-service/src/main/java/cz/fi/muni/pa165/datatransferobject/MemberDTO.java
@@ -5,10 +5,99 @@
  */
 package cz.fi.muni.pa165.datatransferobject;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 /**
  *
- * @author Boris Valentovic - xvalent2
+ * @author Martin Malik <374128@mail.muni.cz>
  */
 public class MemberDTO {
     
+    private long idMember;
+    
+    private String name;
+    
+    private String email;
+    
+    private String address;
+    
+    private Set<LoanDTO> loans = new HashSet<LoanDTO>();
+    
+    public MemberDTO(){    
+    }
+
+    public long getIdMember() {
+        return idMember;
+    }
+
+    public void setIdMember(long idMember) {
+        this.idMember = idMember;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<LoanDTO> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(Set<LoanDTO> loans) {
+        this.loans = loans;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (int) (this.idMember ^ (this.idMember >>> 32));
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Objects.hashCode(this.email);
+        hash = 79 * hash + Objects.hashCode(this.address);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MemberDTO other = (MemberDTO) obj;
+        if (this.idMember != other.idMember) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        return true;
+    }
 }
---
+++ library-service/src/main/java/cz/fi/muni/pa165/service/MemberServiceImpl.java
@@ -0,0 +1,107 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.dao.BookDAO;
+import cz.fi.muni.pa165.dao.MemberDAO;
+import cz.fi.muni.pa165.datatransferobject.BookDTO;
+import cz.fi.muni.pa165.datatransferobject.MemberDTO;
+import cz.fi.muni.pa165.entity.Book;
+import cz.fi.muni.pa165.entity.Member;
+import cz.fi.muni.pa165.service.api.MemberService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Martin Malik <374128@mail.muni.cz>
+ */
+@Transactional
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    MemberDAO memberDAO;
+
+    @Autowired
+    BookDAO bookDAO;
+    
+    @Override
+    public void insertMember(MemberDTO memberDTO) {
+        Member member = DTOEntityManager.memberDTOtoEntity(memberDTO);
+        memberDAO.insert(member);
+    }
+
+    @Override
+    public void updateMember(MemberDTO memberDTO) {
+        Member member = DTOEntityManager.memberDTOtoEntity(memberDTO);
+        memberDAO.update(member);
+    }
+
+    @Override
+    public void deleteMember(MemberDTO memberDTO) {
+        Member member = DTOEntityManager.memberDTOtoEntity(memberDTO);
+        memberDAO.delete(member);
+    }
+
+    @Override
+    public MemberDTO find(MemberDTO memberDTO) {
+        Member member = memberDAO.find(DTOEntityManager.memberDTOtoEntity(memberDTO));
+        return DTOEntityManager.memberEntitytoDTO(member);
+    }
+
+    @Override
+    public MemberDTO findMemberByIdMember(long id) {
+        return DTOEntityManager.memberEntitytoDTO(memberDAO.findMemberByIdMember(id));
+    }
+
+    @Override
+    public List<MemberDTO> findMembersByName(String name) {
+        List<Member> members = memberDAO.findMembersByName(name);
+        return MemberEntityArrayToMemberDTOArray(members);
+    }
+
+    @Override
+    public MemberDTO findMemberByEmail(String email) {
+        return DTOEntityManager.memberEntitytoDTO(memberDAO.findMemberByEmail(email));
+    }
+
+    @Override
+    public List<MemberDTO> findMembersByAddress(String address) {
+        List<Member> members = memberDAO.findMembersByAddress(address);
+        return MemberEntityArrayToMemberDTOArray(members);
+    }
+
+    @Override
+    public List<MemberDTO> findMembersByBook(BookDTO bookDTO) {
+        Book book = bookDAO.find(DTOEntityManager.bookDTOtoEntity(bookDTO));
+        List<Member> members = memberDAO.findMembersByBook(book);
+        return MemberEntityArrayToMemberDTOArray(members);
+    }
+    
+    private List<MemberDTO> MemberEntityArrayToMemberDTOArray(List<Member> members){
+        List<MemberDTO> membersDTO= new ArrayList<MemberDTO>();
+        for(Member member : members){
+            membersDTO.add(DTOEntityManager.memberEntitytoDTO(member));
+        }
+        
+        return membersDTO;
+    }
+
+    public MemberDAO getMemberDAO() {
+        return memberDAO;
+    }
+
+    public void setMemberDAO(MemberDAO memberDAO) {
+        this.memberDAO = memberDAO;
+    }
+
+    public BookDAO getBookDAO() {
+        return bookDAO;
+    }
+
+    public void setBookDAO(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+}
