e017bc06d963f6eff3708a5911c6daeeb2f6dd57
Sergii Pylypenko <serj.pilipenko@gmail.com>
2014-12-19 23:05:36 +0100

    added loging

--- library-web/src/main/java/cz/fi/muni/pa165/library/ExceptionControllerAdvice.java
+++ library-web/src/main/java/cz/fi/muni/pa165/library/ExceptionControllerAdvice.java
@@ -9,16 +9,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
+    private static final Logger log = Logger.getLogger( ExceptionControllerAdvice.class.getName() );
+
     @ExceptionHandler(Exception.class)
-    public ModelAndView exception(Exception e) {
+    public ModelAndView exception(Exception e) throws Exception {
+
 
         ModelAndView mav = new ModelAndView("exception");
         mav.addObject("name", e.getClass().getSimpleName());
         mav.addObject("message", e.getMessage());
 
+        log.log( Level.SEVERE, e.toString(), e);
         return mav;
     }
 }
